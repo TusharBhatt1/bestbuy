0096e1f7cb6a07af898feea3e71ddd66349f3a0d
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-12-22 01:58:14 +0530

    fix - [type error]

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import Banner from "./Components/Banner";
 import ProductsList from "./Components/product/ProductsList";
 import WishList from "./Components/wishlistSideBox/WishList";
-import { getTheme } from "./Others/fetch/getTheme";
-import useThemeStore from "./Others/hooks/useTheme";
 
 export default async function Home() {
   return (
