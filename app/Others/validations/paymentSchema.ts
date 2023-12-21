28bf53aa7d7305cc9f69e7e10f42a354720d0fa9
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-12-21 23:42:09 +0530

    add - [font + colors]

--- app/Others/validations/paymentSchema.ts
+++ app/Others/validations/paymentSchema.ts
@@ -2,25 +2,25 @@ import * as Yup from 'yup';
 
 export const paymentSchema = Yup.object().shape({
   paymentMode: Yup.string().required('Please select a payment mode'),
-  holderName: Yup.string().required('Holder name is required'),
+  holderName: Yup.string().min(5).max(30).required(),
   //@ts-ignore
   upiAddress: Yup.string().when('paymentMode', {
     is: (paymentMode) => paymentMode === 'UPI',
     then: Yup.string()
-      .matches(/^[\w.-]+@[\w.-]+$/, 'Invalid UPI address')
-      .required('UPI address is required'),
+      .matches(/^[\w.-]+@[\w.-]+$/, 'Invalid, ex : xyz@sbi')
+      .required('Required'),
   }),
   //@ts-ignore
   cvvNum: Yup.string().when('paymentMode', {
     is: (paymentMode) => paymentMode === 'CARDS',
-    then: Yup.string().min(3).max(3).required('CVV is required'),
+    then: Yup.string().matches(/^\d{3}$/, 'Card number length is 3 ').required("Required")
   }),
   //@ts-ignore
   cardNumber: Yup.string().when('paymentMode', {
     is: (paymentMode) => paymentMode === 'CARDS',
     then: Yup.string()
-      .matches(/^\d{16}$/, 'Invalid card number')
-      .required('Card number is required'),
+      .matches(/^\d{16}$/, 'Card number length is 16 ')
+      .required('Required'),
   }),
 });
 
