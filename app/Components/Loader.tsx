2bda2076cc9d1484ecccd2af3ada20f1dadda01c
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-12-23 13:53:06 +0530

    add - [app desc]

--- app/Components/Loader.tsx
+++ app/Components/Loader.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
@@ -15,16 +15,17 @@ export default function Loader() {
     return (
       <div className="fixed text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30">
         <div className="bg-black h-[100vh] w-screen flex flex-col items-center justify-center">
-          <div className="border-t-2 text-center border-blue-500 border-solid rounded-full w-4 h-4 animate-spin"></div>
-          <div className="mt-4 text-blue-500  font-bold text-lg">Loading...</div>
-          <div className="flex flex-col mt-2">
-          <p className="text-sm text-pink-400">A NextJS + Typescript + Zustand Web Application by</p> 
-          <p className="text-center font-bold text-red-100">Tushar Bhatt</p> 
+          <div className="border-t-2 text-center border-blue-500 border-solid rounded-full w-8 h-8 animate-spin"></div>
+          <div className="flex mt-8 flex-col">
+            <p className="text-sm text-pink-400">
+              A NextJS + Typescript + Zustand Web Application by
+            </p>
+            <p className="text-center font-bold text-red-100">Tushar Bhatt</p>
           </div>
         </div>
       </div>
     );
   }
 
-  return null; 
+  return null;
 }
