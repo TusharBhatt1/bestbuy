4084edcea23ce57497a76838d0b299454aa29d52
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-12-24 22:57:04 +0530

    change [loader css]

--- app/Components/Loader.tsx
+++ app/Components/Loader.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 export default function Loader() {
-  const [showLoader, setShowLoader] = useState(true);
+  const [showLoader, setShowLoader] = useState(true)
 
   useEffect(() => {
     setTimeout(() => {
@@ -16,16 +16,14 @@ export default function Loader() {
       <div className="fixed text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30">
         <div className="bg-black h-[100vh] w-screen flex flex-col items-center justify-center">
           <div className="border-t-2 text-center border-blue-500 border-solid rounded-full w-8 h-8 animate-spin"></div>
-          <div className="flex mt-8 flex-col">
-            <p className="text-sm text-pink-400">
+            <p className="text-sm font-bold mt-4 text-pink-400">
               A NextJS + Typescript + Zustand Web Application by
             </p>
-            <p className="text-center font-bold text-red-100">Tushar Bhatt</p>
-          </div>
+            <p className="text-center font-extrabold text-red-100">Tushar Bhatt</p>
         </div>
       </div>
     );
   }
 
-
+  return null;
 }
