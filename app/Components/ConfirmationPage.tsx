9de55eb48d8655d315844a3f7a8f2c3a3b21edcb
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-12-26 00:12:03 +0530

    change - [confirmation page unauth]

--- app/Components/ConfirmationPage.tsx
+++ app/Components/ConfirmationPage.tsx
@@ -10,17 +10,14 @@ export default function ConfirmationPage(){
   const router = useRouter();
    
 
-  useEffect(()=>{
-    if (paymentMethod === "") {
-     unAuth();
-    }
-  },[router])
+  
   const unAuth = () => {
     router.push("/");
     toast.error("Unauthorized");
     return;
   };
   
+  if(paymentMethod==="")unAuth()
 
   const onClick = () => {
     router.push("/");
