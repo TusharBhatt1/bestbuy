de18fd84d8a0885bf44a45ef0032412451af765e
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-12-29 00:08:09 +0530

    add - [loading ui to dynamic product details]

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,5 +1,12 @@
 import dynamic from "next/dynamic";
-const DynamicProductDetails = dynamic(() => import("./GetProduct"));
+import { FiLoader } from "react-icons/fi";
+const DynamicProductDetails = dynamic(() => import("./GetProduct"),{
+  loading:()=>(
+    <div className='h-80 w-full flex justify-center items-center bg-slate-200'>
+    <FiLoader className="mt-10 animate-spin"/>
+  </div>
+  )
+});
 
 export default function page({ params }: { params: { id: number } }) {
   const { id } = params;
