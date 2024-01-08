8849f00ba8b274e7e8625c7597142fc48e1c96f1
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-09 02:07:43 +0530

    fix - [SSR error]

--- app/Components/OrderSummary.tsx
+++ app/Components/OrderSummary.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { FaUser, FaMapMarkerAlt, FaPhone, FaShoppingCart } from "react-icons/fa";
+import {
+  FaUser,
+  FaMapMarkerAlt,
+  FaPhone,
+  FaShoppingCart,
+} from "react-icons/fa";
 import useUserPaymentData from "../Others/hooks/useUserPaymentData";
 import useCartDetails from "../Others/hooks/useCartDetails";
 import Image from "next/image";
@@ -14,26 +19,33 @@ export default function OrderSummary() {
       <div className="flex-1 gap-4">
         <p className="font-bold text-center">User Details</p>
 
-       <div className="flex gap-4 mb-4 justify-center items-start">
-       <Image src={URL.createObjectURL(userdetails.profile)} className="sm:block hidden h-10 w-10 rounded-full mb-2"  height={40} width={40} alt={userdetails.name}/>
-
-        <div className="flex flex-col gap-2 mt-4 text-sm">
-          <p className="flex items-center">
-            <FaUser className="mr-2" />
-            <span className="font-semibold">Name: </span>{" "}
-            <span className="text-slate-500">  {userdetails.name}</span>
-          </p>
-          <p className="flex items-center">
-            <FaMapMarkerAlt className="mr-2" />
-            <span className="font-semibold">Address: </span>
-            <span className="text-slate-500"> {userdetails.address}</span>
-          </p> 
-          <p className="flex items-center">
-            <FaPhone className="mr-2" />
-            <span className="font-semibold">Contact: </span>
-            <span className="text-slate-500"> {userdetails.contact}</span>
-          </p>
-        </div>
+        <div className="flex gap-4 mb-4 justify-center items-start">
+          {typeof document !== "undefined" && (
+            <Image
+              src={URL.createObjectURL(userdetails.profile)}
+              className="sm:block hidden h-10 w-10 rounded-full mb-2"
+              height={40}
+              width={40}
+              alt={userdetails.name}
+            />
+          )}
+          <div className="flex flex-col gap-2 mt-4 text-sm">
+            <p className="flex items-center">
+              <FaUser className="mr-2" />
+              <span className="font-semibold">Name: </span>{" "}
+              <span className="text-slate-500"> {userdetails.name}</span>
+            </p>
+            <p className="flex items-center">
+              <FaMapMarkerAlt className="mr-2" />
+              <span className="font-semibold">Address: </span>
+              <span className="text-slate-500"> {userdetails.address}</span>
+            </p>
+            <p className="flex items-center">
+              <FaPhone className="mr-2" />
+              <span className="font-semibold">Contact: </span>
+              <span className="text-slate-500"> {userdetails.contact}</span>
+            </p>
+          </div>
         </div>
       </div>
 
@@ -48,10 +60,7 @@ export default function OrderSummary() {
               key={item.id}
               className="flex justify-between items-center text-xs text-slate-500"
             >
-              <li className="flex items-center ">
-              
-                { item.title}
-              </li>
+              <li className="flex items-center ">{item.title}</li>
               <span>({item.quantity})</span>
             </div>
           ))}
