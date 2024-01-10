6a151b206bc07ff01d8996b8c7b1d3c4de16d9bf
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-10 20:34:58 +0530

    responsive-design

--- app/checkout/CheckoutPage.tsx
+++ app/checkout/CheckoutPage.tsx
@@ -63,8 +63,8 @@ export default function CheckoutPage({
     toast.error("Removed");
   };
   return (
-    <div className="flex gap-2 flex-col justify-center items-center sm:px-12">
-      <div className="flex flex-col gap-2  p-2">
+    <div className="flex gap-2 flex-col justify-center items-center px-14 sm:px-12">
+      <div className="flex flex-col justify-center gap-2 px-8 py-2 shadow-md rounded-md">
         <div className="grid grid-cols-4  justify-center items-center text-center font-bold text-slate-500">
           <p>Product</p>
           <p>Price</p>
@@ -82,7 +82,7 @@ export default function CheckoutPage({
             >
               <div className="flex justify-center items-center">
                 <Link href={`/product/${item.id}`}>
-                  <div className="flex flex-col sm:flex-row justify-start w-[24vw] items-center p-2 gap-4 text-xs">
+                  <div className="flex flex-col sm:flex-row justify-start w-[24vw] items-center p-2 gap-2 text-xs">
                     <Image
                       src={item.image}
                       width={20}
