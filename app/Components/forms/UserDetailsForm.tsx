0384a8974cbbb26c6d7932a0a2a29aef872fa0e4
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-11 23:01:23 +0530

    increase - [gap]

--- app/Components/forms/UserDetailsForm.tsx
+++ app/Components/forms/UserDetailsForm.tsx
@@ -104,7 +104,7 @@ export default function UserDetailsForm() {
   return (
     <div className="flex justify-center items-center sm:p-4 ">
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="max-w-xl justify-center items-center p-4 rounded-md shadow-md grid grid-cols-2 gap-4">
+      <div className="max-w-xl justify-center items-center p-4 rounded-md shadow-md grid grid-cols-2 gap-2">
         {formConfig?.map((row, id) => {
           const name: any = row?.name
           const prop = {
